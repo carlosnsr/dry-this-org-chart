7989d65bba885b6f7da4fe6aca740331ddda226c
Alex <amlcodes>
2017-03-09 13:51:22 -0700

    need a protype or extends to add elon as a manager for all emp

--- org_chart.js
+++ org_chart.js
@@ -5,85 +5,41 @@ let elon = {
 }
 
 
-function New_hire(name, email, position, project){
+function Employee(name, email, position, project, manager){
   this.name = name;
   this.email = email;
   this.position = position;
-  this.projec = project;
+  this.project = project;
+  this.manager = manager;
 }
 
-let alan = new New_hire("Alan Turing", "grace.hopper@musky.com", "Computer Scientist", "Cryptography")
-let grace = new New_hire("Grace Hopper", "alan.turing@musky.com", "Computer Scientist", "Compilers")
-let donald = new New_hire("Donald Knuth",, "donald.knuth@musky.com", "Mathematician", "Algorithm Analysis")
-
-// let new_hires = [
-//   {
-//     name: "Alan Turing",
-//     email: "alan.turing@musky.com",
-//     position: "Computer Scientist",
-//     project: "Cryptography"
-//   },
-//   {
-//     name: "Grace Hopper",
-//     email: "grace.hopper@musky.com",
-//     position: "Computer Scientist",
-//     project: "Compilers"
-//   },
-//   {
-//     name: "Donald Knuth",
-//     email: "donald.knuth@musky.com",
-//     position: "Mathematician",
-//     project: "Algorithm Analysis"
-//   }
-// ]
-
-// assign each a manager
-
-new_hires.prototype.manger = elon
-
-// alan.manager = elon
-// grace.manager = elon
-// donald.manager = elon
+let alan = new Employee("Alan Turing", "grace.hopper@musky.com", "Computer Scientist", "Cryptography", elon)
+let grace = new Employee("Grace Hopper", "alan.turing@musky.com", "Computer Scientist", "Compilers", elon)
+let donald = new Employee("Donald Knuth", "donald.knuth@musky.com", "Mathematician", "Algorithm Analysis", elon)
 
 
-if (!new_hires.skills){
-  // send to training
-  if (!alan.skills) {
-    alan.skills = []
-  }
-  alan.skills.push("Mathematics")
+alan.skills = []
+alan.skills.push("Mathematics")
 
-  if (!grace.skills) {
-    grace.skills = []
-  }
-  grace.skills.push("Cobol")
+grace.skills = []
+grace.skills.push("Cobol")
 
-  if (!donald.skills) {
-    donald.skills = []
-  }
-  donald.skills.push("Computational Complexity")
-}
+donald.skills = []
+donald.skills.push("Computational Complexity")
 
-// whoops!  new person hired later on, do the same again for him
-// let new_hire = {
-//   name: "Tim Berners-Lee",
-//   email: "tim.berners_lee@musky.com",
-//   position: "Computer Science",
-//   project: "Networks"
-// }
-let tim = new New_hire("Tim Berners-Lee", "tim.berners_lee@musky.com", "Computer Science", "Networks")
+let tim = new Employee("Tim Berners-Lee", "tim.berners_lee@musky.com", "Computer Science", "Networks")
 tim.manager = elon
-
-if (!tim.skills) {
-  tim.skills = []
-}
+tim.skills = []
 tim.skills.push("Protocols")
 
-console.log(`${alan.name} is a ${alan.position} working on ${alan.project}.`)
-console.log(`He can be reached at ${alan.email}`)
-console.log(`${grace.name} is a ${grace.position} working on ${grace.project}.`)
-console.log(`She can be reached at ${grace.email}`)
-console.log(`${donald.name} is a ${donald.position} working on ${donald.project}.`)
-console.log(`He can be reached at ${donald.email}`)
-console.log(`${tim.name} is a ${tim.position} working on ${tim.project}.`)
-console.log(`He can be reached at ${tim.email}`)
+let employees = [alan, grace, donald, tim]
+
+console.log(employees);
+
+
+
+for (var i = 0; i < employees.length; i++) {
+  let emp = employees[i];
+  console.log(`${emp.name} is a ${emp.position} working on ${emp.project}.`)
+  console.log(`He can be reached at ${emp.email}`)
+}
